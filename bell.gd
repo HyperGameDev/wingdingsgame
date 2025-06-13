846cc981e935c59a16df80012bbe36347537f06b
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-06-13 18:07:17 -0500

    Bells work good

---
+++ bell.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@onready var label_bell: Label = $Label_Bell
+
+@onready var label_highlight: Label = $Label_Highlight
+
+var player_seen: bool = false
+
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	
+func _on_body_entered(body) -> void:
+	if not player_seen: 
+		if body is Player:
+			player_seen = true
+			label_highlight.visible = true
+			
+			label_bell.modulate = Color(Color(0.62, 0.498, 0.161))
+			
+
+func _on_body_exited(body) -> void:
+	if body is Player:
+		player_seen = false
